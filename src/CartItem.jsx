9826ab0fe1a3d671e1066332af076b1ac3b3471a
shani-r <ramroopshani@gmail.com>
2025-01-20 18:09:26 -0500

    initialcommit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,50 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+            let totalCost = 0;
+
+            cart.forEach((item) => {const itemCost = parseItemCostToInterger(item.cost);
+            totalCost += itemCost * item.quantity;
+            });
+            return totalCost;
   };
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    const updatedItem = {...item};
+    updatedItem.quantity++;
+    dispatch(updateQuantity(updatedItem));
   };
 
   const handleDecrement = (item) => {
-   
+   const updatedItem = {...item};
+
+   if (updatedItem.quantity === 1) {
+    dispatch(removeItem(updatedItem));
+   } else {
+    updatedItem.quantity--;
+    dispatch(updateQuantity(updatedItem));
+   }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let totalCost = 0;
+    const itemCost = parseItemCostToInterger(item.cost);
+    totalCost = item.quantity * itemCost;
+
+    return totalCost;
   };
 
   return (
@@ -57,12 +80,10 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
 };
 
 export default CartItem;
-
-
--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,16 +4,35 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    numOfItems: 0
   },
   reducers: {
     addItem: (state, action) => {
-    
+        const { name, image, cost } = action.payload;
+        const existingItem = state.items.find(item => item.name === name);
+        if (existingItem) {
+          existingItem.quantity++;
+        } else {
+          state.items.push({ name, image, cost, quantity: 1 });
+        }
+        state.numOfItems += 1;
     },
     removeItem: (state, action) => {
+        const { name, quantity } = action.payload;
+            state.items = state.items.filter(item => item.name !== action.payload);
+            state.numOfItems -= quantity;
+            
+            if (state.numOfItems < 0) {
+                state.numOfItems = 0;
+            }
     },
     updateQuantity: (state, action) => {
-
-    
+        const { name, quantity } = action.payload;
+        const itemToUpdate = state.items.find(item => item.name === name);
+        if (itemToUpdate) {
+          itemToUpdate.quantity = quantity;
+        }
+        
     },
   },
 });
